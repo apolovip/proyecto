7d82994020ed776869240d95f20c781890c80923
Rafael <alvarezrafaelat@gmail.com>
2025-09-22 16:18:48 -0400

    prueba de php

--- src/view/plantilla.php
+++ src/view/plantilla.php
@@ -2,11 +2,20 @@
 
 session_start();
 
-//inlcuir heaader, footer y demas elementos
+$exepciones = ["404-view.php"];
+
+if (!in_array($vista, $exepciones)) {
+    include_once("assets/layouts/links.php");
+    include_once("assets/layouts/header.php");
+    include_once("html/" . $vista);
+    include_once("assets/layouts/footer.php");
+    include_once("assets/layouts/scripts.php");
+    exit();
+}
+
 include_once("assets/layouts/links.php");
-include_once("assets/layouts/header.php");
 include_once("html/" . $vista);
-include_once("assets/layouts/footer.php");
 include_once("assets/layouts/scripts.php");
 
 
+//inlcuir heaader, footer y demas elementos
